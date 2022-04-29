fee1b6666f5e4d29a5b06d6596ff608801540764
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-04-29 23:02:57 +0400

    feat: add new slider

--- pages/main/script.js
+++ pages/main/script.js
@@ -2,4 +2,28 @@ function btnOurFriends(e){
     window.location.hash="ourFriends";
     console.log("rame")
 }
+const cardWrap = document.querySelector(".card-wrap");
+const card = document.getElementsByClassName("card");
 
+let counter = 0;
+
+const leftArrow = document.querySelector(".leftArrow");
+const rightArrow = document.querySelector(".rightArrow");
+
+leftArrow.addEventListener("click", ()=>{
+    if(counter == 0) {
+        counter=card.length-2
+    }if(counter !== 0){
+        counter--;
+    }
+    cardWrap.style.transform= 'translate('+(counter) *  -16.5 +'%)';
+});
+
+rightArrow.addEventListener('click',()=>{
+    if(counter == card.length-3){
+      counter=-1;
+  }if(counter <= card.length-4){
+      counter++
+  }
+  cardWrap.style.transform= 'translate('+(counter) *  -16.5 +'%)';
+})
